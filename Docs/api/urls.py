757b82394610ea3d8c987d6f2e4f34fac668dc31
alixaprodev <haxratali0@gmail.com>
2023-11-04 23:47:34 +0500

    added crud to user table

--- Docs/api/urls.py
+++ Docs/api/urls.py
@@ -4,5 +4,27 @@ from . import views
 
 urlpatterns=[
     path('', views.home, name='home'),
-]
+    
+    # path('users/',views.create_user, name='create-user'),
+    # path('users/',views.list_users, name='list-users'),
+    # path('users/<int:id>',views.update_user, name='update-user'),
+    # path('users/<int:id>',views.update_user_partial, name='update-user-partial'),
+    # path('users/<int:id>',views.delete_user, name='delete-user'),
 
+    path('users/create', views.create_user, name='create-user'),
+
+    # Get a user by ID using GET
+    path('users/<int:user_id>/', views.get_user, name='get-user'),
+
+    # Update a user by ID using PUT
+    path('users/<int:user_id>/update', views.update_user, name='update-user'),
+
+    # Partially update a user by user_id using PATCH
+    path('users/<int:user_id>/partial/', views.update_user_partial, name='update-user-partial'),
+
+    # Delete a user by user_id using DELETE
+    path('users/<int:user_id>/delete/', views.delete_user, name='delete-user'),
+
+    # List all users using GET
+    path('users/', views.list_users, name='list-users'),
+]
--- Docs/api/views.py
+++ Docs/api/views.py
@@ -1,13 +1,94 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-
+from .serializers import CustomUserSerializer
+from .models import CustomUser
+from rest_framework import status
 @api_view(['GET'])
 def home(request):
     data = {
         "urls": {
             "": reverse("home", request=request),
-            # Add more URLs here for your views and endpoints
+            "list-users": reverse("list-users", request=request),
+            "get-users <id=int>": reverse("get-user", kwargs={'id':1}, request=request),
         }
     }
     return Response(data)
+
+@api_view(['POST'])
+def create_user(request):
+    if request.method == 'POST':
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'success': True, 'data': serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({'success': False, 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'success': False, 'error': 'Method Not Allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['GET'])
+def get_user(request, user_id):
+    try:
+        user = CustomUser.objects.get(id=user_id)
+    except CustomUser.DoesNotExist:
+        return Response({'success': False, 'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CustomUserSerializer(user)
+        return Response({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+    return Response({'success': False, 'error': 'Method Not Allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+
+
+@api_view(['PUT'])
+def update_user(request, user_id):
+    try:
+        user = CustomUser.objects.get(id=user_id)
+    except CustomUser.DoesNotExist:
+        return Response({'success': False, 'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = CustomUserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+        return Response({'success': False, 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'success': False, 'error': 'Method Not Allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['PATCH'])
+def update_user_partial(request, user_id):
+    try:
+        user = CustomUser.objects.get(id=user_id)
+    except CustomUser.DoesNotExist:
+        return Response({'success': False, 'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PATCH':
+        serializer = CustomUserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+        return Response({'success': False, 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'success': False, 'error': 'Method Not Allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['DELETE'])
+def delete_user(request, user_id):
+    try:
+        user = CustomUser.objects.get(id=user_id)
+    except CustomUser.DoesNotExist:
+        return Response({'success': False, 'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        user.delete()
+        return Response({'success': True, 'message': 'User deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
+    return Response({'success': False, 'error': 'Method Not Allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['GET'])
+def list_users(request):
+    if request.method == 'GET':
+        users = CustomUser.objects.all()
+        serializer = CustomUserSerializer(users, many=True)
+        return Response({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+    return Response({'success': False, 'error': 'Method Not Allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+    
+    
